a218e1b56511080488be8096d2eb063ad67fdaf7
Yago Santos <yagojf27@gmail.com>
2020-11-11 17:59:26 -0300

    organizando a pasta

---
+++ curso3 js intro/js/script.js
@@ -0,0 +1,11 @@
+//alert("Hello World!");
+
+function pulaLinha(){
+    document.write("<br>");
+}
+
+var nome = "Yago Santos";
+
+document.write(nome);
+pulaLinha();
+document.write(nome.replace("Santos", "Ribeiro"));
